e3db1f96c543345881cdaf389233ab43e40bedb2
persianpros <persianpros@yahoo.com>
2020-04-29 10:15:38 +0430

    py files: E101 and E121 PEP8 cleanup
    Indentation contains mixed spaces and tabs (E101)
    Continuation line under-indented for hanging indent (E121)

--- plugin/__init__.py
+++ plugin/__init__.py
@@ -6,15 +6,15 @@ from Components.Language import language
 from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 
 def localeInit():
-	environ["LANGUAGE"] = language.getLanguage()[:2]
-	bindtextdomain("ServiceHisilicon", resolveFilename(SCOPE_PLUGINS, \
-		"SystemPlugins/ServiceHisilicon/locale"))
+    environ["LANGUAGE"] = language.getLanguage()[:2]
+    bindtextdomain("ServiceHisilicon", resolveFilename(SCOPE_PLUGINS, \
+            "SystemPlugins/ServiceHisilicon/locale"))
 
 def _(txt):
-	t = dgettext("ServiceHisilicon", txt)
-	if t == txt:
-		t = gettext(txt)
-	return t
+    t = dgettext("ServiceHisilicon", txt)
+    if t == txt:
+        t = gettext(txt)
+    return t
 
 localeInit()
 language.addCallback(localeInit)
