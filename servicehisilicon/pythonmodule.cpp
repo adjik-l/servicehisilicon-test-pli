f1b9c130c16ea269bef6998d52f68c4a3ba342fd
persianpros <persianpros@gmail.com>
2020-10-23 15:33:57 +0330

    Be compatible with python 2 also

--- servicehisilicon/pythonmodule.cpp
+++ servicehisilicon/pythonmodule.cpp
@@ -1,11 +1,12 @@
 #include "servicehisilicon.h"
-
+#include <Python.h>
 
 static PyMethodDef servicehisiliconMethods[] =
 {
 	{NULL,NULL,0,NULL}
 };
 
+#if PY_MAJOR_VERSION >= 3
 static struct PyModuleDef moduledef = {
 	PyModuleDef_HEAD_INIT,
 	"servicehisilicon",         /* m_name */
@@ -22,3 +23,10 @@ PyMODINIT_FUNC PyInit_servicehisilicon(void)
 {
 	return PyModule_Create(&moduledef);
 }
+#else
+PyMODINIT_FUNC
+initservicehisilicon(void)
+{
+	Py_InitModule("servicehisilicon", servicehisiliconMethods);
+}
+#endif
